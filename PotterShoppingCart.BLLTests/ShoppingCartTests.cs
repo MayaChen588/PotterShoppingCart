1d6b0baa6c53a33d3d47f6f669c390ef58e9f6d3
mayachen588 <maya.chencc@gmail.com>
2016-01-22 16:45:51 +0800

    購買二本價格應為190元-失敗

---
+++ PotterShoppingCart.BLLTests/ShoppingCartTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PotterShoppingCart.BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace PotterShoppingCart.BLL.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void Test_購買一本價格應為100元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = 100;
+            var actual = target.CalTotalAmount(1);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+
+        [TestMethod()]
+        public void Test_購買二本價格應為190元()
+        {
+            // arrange
+            var target = new ShoppingCart();
+
+            // act
+            var expented = 190;
+            var actual = target.CalTotalAmount(2);
+            // assert
+
+            Assert.AreEqual(expented, actual);
+        }
+    
+
+    }
+}
