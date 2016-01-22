ea57bdd9abf4809d35ea6be0257a05ac8902564d
mayachen588 <maya.chencc@gmail.com>
2016-01-22 16:44:34 +0800

    第一集買了一本，其他都沒買，價格應為100*1=100元

---
+++ PotterShoppingCart.BLL/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.BLL
+{
+    public class ShoppingCart
+    {
+        private decimal _price = 100;
+
+        public decimal CalTotalAmount(int quantity)
+        {
+            return quantity * _price;
+        }
+    }
+}
